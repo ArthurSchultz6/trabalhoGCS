31398d6740715bb2fd3866c9b025ec1760f54a21
Arthur Schultz <arthurschultz@MacBook-Pro-de-Arthur-2.local>
2025-09-26 18:53:44 -0300

    funcionalidade para ler nome/usuario

--- Programa.java
+++ Programa.java
@@ -1,10 +1,18 @@
 
 public class Programa {
-    public static void main(String[] args) {
-        string nomeUsuario = "Aluno de GCS";
-        saudar(nomeUsuario);
-    }
+
     public static void saudar(String nome) {
         System.out.println("Olá, " + nome + "!");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite seu nome: ");
+        String nomeUsuario = scanner.nextLine();
+        saudar(nomeUsuario);
+
+
     }
 }
